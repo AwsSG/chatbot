996b4b6f703985c5307ce6de5e9b5c2dda425b73
AwsSG <aws.sabah.gheni@gmail.com>
2020-10-08 23:02:19 +0000

    Added first level of questions in the answers.js and added if statement to compare the selected choice with the answers for a match

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -1,3 +1,5 @@
-$(document).ready(function(){
-    $("h5").css("color", "lightgreen");
-});
+    var answerList = {
+        "I need help with my bill": ["My bill amount is incorrect", "My bill did not arrive yet"],
+        "I have a technical problem": ["The service is down", "Equipment is not working"],
+        "I have question about something else": ["Can I get eFiber?","I want to change my number"]
+    };
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,23 @@
 $(document).ready(function () {
   $(".choice").click(function () {
     $(this).removeClass("choice").addClass("selected");
-    $(".choice").fadeOut("fast", function () { //https://stackoverflow.com/questions/553402/how-to-fadeout-remove-a-div-in-jquery
+    $(".choice").fadeOut("fast", function () {
       $(this).remove();
     });
+
+    for (let i = 0; i < Object.keys(answerList).length; i++) { 
+    if (Object.keys(answerList)[i] === $(".selected").text()) {
+
+        console.log("I found a match!");
+    };
+    };
+        
   });
 
 
 
 
 });
+
+// souce for fading out and removing elements:
+//https://stackoverflow.com/questions/553402/how-to-fadeout-remove-a-div-in-jquery 
