48e259478d58ab1b04da2c11e88d15d0372b9289
AwsSG <aws.sabah.gheni@gmail.com>
2020-10-09 21:53:15 +0000

    created a loop that looks for a matching key (to the selected question) then loop through and append each of its values

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
   $(".choice").click(function () {
     $(this).removeClass("choice").addClass("selected");
+
+    // fading out and removing the unselected choices(p elements)
     $(".choice").fadeOut("fast", function () {
       $(this).remove();
-    });
+    }); 
 
+    // looping through answerList to find a key match, then loop through and append each value for the matching key 
     for (let i = 0; i < Object.keys(answerList).length; i++) { 
     if (Object.keys(answerList)[i] === $(".selected").text()) {
-
-        console.log("I found a match!");
+        for (let choiceCounter = 0; answerList[$(".selected").text()].length > choiceCounter; choiceCounter ++) {
+            $(".chatbox").append('<p class="question">'+answerList[$(".selected").text()][choiceCounter]+'</p>');
+        };
     };
     };
         
