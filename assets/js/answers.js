95c4d635c66303ff6034afd074eb089f482ae062
AwsSG <aws.sabah.gheni@gmail.com>
2020-10-11 20:02:59 +0000

    Added questions to the click handler, added reset button which restores the chatbox div contents to its original

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -7,7 +7,7 @@
                 "Lower than expected": ["I cancelled a service","I recieved a credit to my account"],
                     "I cancelled a service": ["Dear customer,<br> When you cancel one of your service, you will no longer be billed for that service. Therefore, your total bill will not include the charges for the cancelled service."],
                     "I recieved a credit to my account": ["Dear customer,<br> When your account gets credited, this will be reflected on your bill by bringing the billed amount down by the amount you were credited. Meaning that the billing is the same as usual, but the amount for this bill is lowered because of the credit you received."],
-            "My bill did not arrive yet": ["By post","by email"],
+            "My bill did not arrive yet": ["By post","By email"],
                 "By post":["I asked for paperless bill","I did not"],
                     "I asked for paperless bill":["Dear customer,<br> When you ask to change the way you receive your bill, make sure that you provided your email accurately. If there is any typos, your bill may have been sent to the wrong email.<br> Alternatively, you can see your bill by logging in your MyEir account at any time."],
                     "I did not":["Dear customer,<br> Getting your bill by post might get delayed from the post service, we suggest that you wait few more days for the bill to arrive. <br>If the bill did not arrive and it has been more than 14 days, we suggest that  you talk to an agent by calling 1901 from Monday to Friday between 09:00 and 20:00."],
@@ -18,14 +18,14 @@
         "I need help with something else": ["Can I get eFiber? (not complete)","I want to change my number (not complete)"]
     };
 
-   /* var questionsList = {
-        "I need help with my bill": "What is the problem you are having with your bill?",
-            "My bill amount is incorrect":"Is your bill higher or lower than what you expected?",
-                "Higher than expected":"Is it your first bill?",
-                "Lower than expected":"Did you cancel any services?",
-            "My bill did not arrive yet": "How do you usually get your bill?",
-                "By post":"Did you change or asked to get your bill by email?",
-                "By email":"Did you change or asked to get your bill by post?",
-        "I have a technical problem": "What technical problem are you having?",
-        "I have question about something else": "What is your question is about?"
-    }; */
+   var questionsList = {
+        "I need help with my bill": "Ok, how can I help you with your bill?",
+            "My bill amount is incorrect":"Alright, is your bill higher or lower than what you expected?",
+                "Higher than expected":"I see, is it your first bill?",
+                "Lower than expected":"That is usually a good thing! Did you cancel any services?",
+            "My bill did not arrive yet": "Hmmm ... How do you usually get your bill?",
+                "By post":"Ok, can you tell me if you changed (or asked) to get your bill by email instead of post?",
+                "By email":"Well, did you change (or asked) to get your bill by post instead of email?",
+        "I have a technical problem": "Oh those are annoying, as a bot I know that very well.<br>What technical problem are you having?",
+        "I need help with something else": "Even if it is about something else, I will do my best to help you.<br>What is your question about?"
+    };
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,29 +1,44 @@
 $(document).ready(function () {
 
+var reset = $(".chatbox").html();
 
   $(".chatbox").on('click',".choice" ,function () {
     $(this).removeClass("choice").addClass("selected");
     $(".choice").remove();
     
     // fading out and removing the unselected choices(p elements)
-    /* $(".choice").fadeOut("fast", function () {
+    /*$(".choice").slideUp("fast", function () {
       $(this).remove();
-    });  */ // will fix the style later
+    }); */ // will fix the style later
 
     // looping through answerList to find a key match, then loop through and append each value for the matching key 
     for (let question in answerList) {
     if (question === $(".selected").text()) {
+        if (questionsList[question] !== undefined){$(".chatbox").append('<p class="question">'+questionsList[question]+'</p>');};
+ 
+
+        if (answerList[question].length > 1){ //this if statement is to check if the answer is the last answer (no followup question comes after)
         for (let choiceCounter = 0; answerList[$(".selected").text()].length > choiceCounter; choiceCounter ++) {
             $(".chatbox").append('<p class="choice">'+answerList[question][choiceCounter]+'</p>');
             };
+            } else {
+                $(".chatbox").append('<p class="lastAnswer">'+answerList[question]+'</p>');
+            };
         };
     };
-    
+    // source for the animated scrolldown:
+    // https://www.geeksforgeeks.org/how-to-scroll-automatically-to-the-bottom-of-the-page-using-jquery/#:~:text=To%20auto%20scroll%20a%20page,%2D1%3A%20Scroll%20without%20animation.
     $(this).removeClass("selected").addClass("history");
+    $("html, body").animate({ 
+                    scrollTop: $( 
+                      'html, body').get(0).scrollHeight 
+                }, 2000); 
 
   });
 
-
+  $("#restoreAll").click(function(){
+      $(".chatbox").html(reset);
+  })
 
 
 });
