78a7dd146d0468afddbd37aba61e4d143bd6f0a6
AwsSG <aws.sabah.gheni@gmail.com>
2020-10-24 21:32:24 +0000

    Modified the questionList and answerList, and added fadeIn effect to the dynamically added html elements

--- assets/js/answers.js
+++ assets/js/answers.js
@@ -2,11 +2,11 @@
         "I need help with my bill": ["My bill amount is incorrect", "My bill did not arrive yet"],
             "My bill amount is incorrect": ["Higher than expected","Lower than expected"],
                 "Higher than expected": ["Yes","No"],
-                    "Yes": ["Dear customer,<br>It is normal for the first bill to be higher than usual. This is because the bill is calculated for the first month in addition to the days you recieved the service, so that first bill is for more than just the first month. <br>First bill may include: <br>* Activation fees. <br>* Installation fees. <br>* Par period charges. <br><br>Unless aditional usage charges are incurred, your next bill should be the amount that you would expect. <br>Aditional usage charges are incurred when you exceed the monthly allowence."],
-                    "No": ["Dear customer,<br>A higher than usual bill can mean one of two things. Either aditional usage charges were incurred because you exceeded the monthly allowence. Or, you recently added another service/add-on.<br>If you believe that this bill is incorrect, you can talk to an agent by calling 1901 from Monday to Friday between 09:00 and 20:00."],
+                    "Yes": ["Ok, since this is your first bill.<br>It is normal for the first bill to be higher than usual. This is because the bill is calculated for the first month in addition to the days you recieved the service, so that first bill is for more than just the first month. <br>First bill may include: <br>* Activation fees. <br>* Installation fees. <br>* Par period charges. <br><br>Unless aditional usage charges are incurred, your next bill should be the amount that you would expect. <br>Aditional usage charges are incurred when you exceed the monthly allowence."],
+                    "No": ["So, higher than usual bill can mean one of two things.<br> Either aditional usage charges were incurred because you exceeded the monthly allowence. Or, you recently added another service/add-on.<br>If you believe that this bill is incorrect, you can talk to an agent by calling 1901 from Monday to Friday between 09:00 and 20:00."],
                 "Lower than expected": ["I cancelled a service","I recieved a credit to my account"],
-                    "I cancelled a service": ["Dear customer,<br> When you cancel one of your service, you will no longer be billed for that service. Therefore, your total bill will not include the charges for the cancelled service."],
-                    "I recieved a credit to my account": ["Dear customer,<br> When your account gets credited, this will be reflected on your bill by bringing the billed amount down by the amount you were credited. Meaning that the billing is the same as usual, but the amount for this bill is lowered because of the credit you received."],
+                    "I cancelled a service": ["So here is what usually happns in this case.<br> When you cancel one of our service, you will no longer be billed for that service. Therefore, your total bill will not include the charges for the cancelled service."],
+                    "I recieved a credit to my account": ["Here is what this means,<br> When your account gets credited, this will be reflected on your bill by bringing the billed amount down by the amount you were credited. Meaning that the billing is the same as usual, but the amount for this bill is lowered because of the credit you received."],
             "My bill did not arrive yet": ["By post","By email"],
                 "By post":["I asked for paperless bill","I did not"],
                     "I asked for paperless bill":["Dear customer,<br> When you ask to change the way you receive your bill, make sure that you provided your email accurately. If there is any typos, your bill may have been sent to the wrong email.<br> Alternatively, you can see your bill by logging in your MyEir account at any time."],
@@ -14,8 +14,10 @@
                 "By email": ["I asked to get paper bill","No, I didn't"],
                     "I asked to get paper bill": ["Dear customer,<br> When you ask to change the way you receive your bill, make sure that you provided your address details accurately. If there is any typos, your bill may have been sent to the wrong address.<br> Alternatively, you can see your bill by logging in your MyEir account at any time."],
                     "No, I didn't": ["Dear customer,<br> You may have already received your bill by email, however it might have been deleted by mistake. Try to check your deleted emails, or check  the spam folder as it may have been placed there.<br> If your still can't find your bill email, you can see your bill by logging in your MyEir account at any time."],
-        "I have a technical problem": ["The service is down (not complete)", "Equipment is not working (not complete)"],
-        "I need help with something else": ["Can I get eFiber? (not complete)","I want to change my number (not complete)"]
+        "I have a technical problem": ["The service is down", "Equipment is not working"],
+            "The service is down": ["Sorry to hear that!<br>You can check if the service is down in your area on our website. If the service is down, the website will also show details of how long it is expected to be down. Please be aware that any down time in the service will NOT be charged."],
+            "Equipment is not working": ["I am sorry that you are going through such inconvenience.<br>You can Either talk to one of our agent in technical support on 0005, or you can bring it in to one of our shops."],
+        "I have a question about a product I am interested in": ["You can talk to one of our sales agents on 0002 between 09:00 and 17:00 from Monday to Friday."]
     };
 
    var questionsList = {
@@ -27,5 +29,5 @@
                 "By post":"Ok, can you tell me if you changed (or asked) to get your bill by email instead of post?",
                 "By email":"Well, did you change (or asked) to get your bill by post instead of email?",
         "I have a technical problem": "Oh those are annoying, as a bot I know that very well.<br>What technical problem are you having?",
-        "I need help with something else": "Even if it is about something else, I will do my best to help you.<br>What is your question about?"
+        "I have a question about a product I am interested in": "In that case, let me direct you to where you can get answers"
     };
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -14,15 +14,15 @@ var reset = $(".chatbox").html();
     // looping through answerList to find a key match, then loop through and append each value for the matching key 
     for (let question in answerList) {
     if (question === $(".selected").text()) {
-        if (questionsList[question] !== undefined){$(".chatbox").append('<p class="question">'+questionsList[question]+'</p>');};
+        if (questionsList[question] !== undefined){$(".chatbox").append('<p class="question">'+questionsList[question]+'</p>');}; //if there is a qustion in the question list for the selected text, then append the question.
  
 
         if (answerList[question].length > 1){ //this if statement is to check if the answer is the last answer (no followup question comes after)
-        for (let choiceCounter = 0; answerList[$(".selected").text()].length > choiceCounter; choiceCounter ++) {
-            $(".chatbox").append('<p class="choice">'+answerList[question][choiceCounter]+'</p>');
+        for (let choiceCounter = 0; answerList[$(".selected").text()].length > choiceCounter; choiceCounter ++) { //loop to add each one of the followup  questions
+            $(".chatbox").append($('<p class="choice">'+answerList[question][choiceCounter]+'</p>').hide().fadeIn("slow")); //append new html element with fadeIn effect
             };
-            } else {
-                $(".chatbox").append('<p class="lastAnswer">'+answerList[question]+'</p>');
+            } else { // for the last answer
+                $(".chatbox").append($('<p class="lastAnswer">'+answerList[question]+'</p>').hide().fadeIn("slow"));
             };
         };
     };
